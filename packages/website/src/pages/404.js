174145ea7037147df7bd7986c822c0b20c5687fe
Riley Davis <riley.davis@ca.com>
2017-08-02 14:17:12 -0600

    WIP: a 404 page

--- packages/website/src/pages/404.js
+++ packages/website/src/pages/404.js
@@ -2,6 +2,11 @@ import React from "react"
 
 export default () =>
   <div>
+    <p style={{
+      fontSize: '300px',
+      margin: 0,
+      fontWeight: 'bold'
+    }}>404</p>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn't exist... the sadness.</p>
   </div>
